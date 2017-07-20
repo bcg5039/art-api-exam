4377890de8df6356266d674eef537c8caad7c40f
bcg5039 <bcg5039@gmail.com>
2017-07-20 09:31:50 -0600

    added readme fixed load-index

---
+++ load-index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const PouchDB = require('pouchdb')
+PouchDB.plugin(require('pouchdb-find'))
+const db = new PouchDB(process.env.COUCHDB_URL + process.env.COUCHDB_NAME)
+
+db
+  .createIndex({
+    index: { fields: ['type'] }
+  })
+  .then(() => console.log("created Index on the 'type' property"))
+  .catch(err => console.log(err))
+
+db
+  .createIndex({
+    index: { fields: ['movement'] }
+  })
+  .then(() => console.log("created Index on the 'movement' property"))
+  .catch(err => console.log(err))
