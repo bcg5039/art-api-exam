8a1cffccfc477982b31beacda192ec98881a08d4
bcg5039 <bcg5039@gmail.com>
2017-07-19 20:45:59 -0600

    step 3 completed

--- app.js
+++ app.js
@@ -1,7 +1,119 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5000
+const dal = require('./dal.js')
+const { pathOr, keys } = require('ramda')
+const bodyParser = require('body-parser')
+const HTTPError = require('node-http-error')
+const checkRequiredFields = require('./lib/check-required-fields')
+const checkPaintingReqFields = checkRequiredFields([
+  'name',
+  'movement',
+  'artist',
+  'yearCreated',
+  'museum'
+])
+app.use(bodyParser.json())
 
+//sample data
+// {
+//   _id: 'painting_starry_night',
+//   name: 'The Starry Night',
+//   type: 'painting',
+//   movement: 'post-impressionism',
+//   artist: 'Vincent van Gogh',
+//   yearCreated: 1889,
+//   museum: { name: 'Museum of Modern Art', location: 'New York' }
+// }
+
+///////////welcome response////////////
 app.get('/', function(req, res, next) {
   res.send('Welcome to the Art API. Manage all the paintings.')
 })
+//////////////////////////////////////////
+
+//CREATE///////////
+app.post('/art/paintings', function(req, res, next) {
+  const painting = pathOr(null, ['body'], req)
+  const checkResults = checkPaintingReqFields(painting)
+
+  if (checkResults.length > 0) {
+    return next(
+      new HTTPError(
+        400,
+        'Painting is missing required fields in the request body.',
+        { fields: checkResults }
+      )
+    )
+  }
+  dal.createPainting(painting, function(err, result) {
+    if (err) return next(new HTTPError(err.status, err.message, err))
+    res.status(201).send(result)
+  })
+})
+//////////////////
+
+////////////////READ//////////
+app.get('/art/paintings/:id', function(req, res, next) {
+  const paintingId = pathOr(null, ['params', 'id'], req)
+
+  if (paintingId) {
+    dal.getPainting(paintingId, function(err, doc) {
+      if (err) return next(new HTTPError(err.status, err.message, err))
+      res.status(200).send(doc)
+    })
+  } else {
+    return next(new HTTPError(400, 'There is no painting id in path!'))
+  }
+})
+//////////////////////////////
+
+////////////////////UPDATE///////////////
+app.put('/art/paintings/:id', function(req, res, next) {
+  const paintingId = pathOr(null, ['params', 'id'], req)
+  const body = pathOr(null, ['body'], req)
+  if (!body || keys(body).length === 0)
+    return next(new HTTPError(400, 'There is no painting in the request body!'))
+
+  dal.updatePainting(body, function(err, response) {
+    if (err) return next(new HTTPError(err.status, err.message, err))
+    res.status(200).send(response)
+  })
+})
+/////////////////////////////////////////
+
+//////////////////DELETE///////////////
+app.delete('/art/paintings/:id', function(req, res, next) {
+  const paintingId = pathOr(null, ['params', 'id'], req)
+
+  dal.deletePainting(paintingId, function(err, response) {
+    if (err) return next(new HTTPError(err.status, err.message, err))
+    res.status(200).send(response)
+  })
+})
+//////////////////////////////////////
+
+///////////////LIST///////////////////
+app.get('/art/paintings', function(req, res, next) {
+  const filter = pathOr(null, ['query', 'filter'], req)
+  const limit = pathOr(5, ['query', 'limit'], req)
+  const lastItem = pathOr(null, ['query', 'lastItem'], req)
+
+  dal.listPaintings(filter, lastItem, Number(limit), function(err, data) {
+    if (err) return next(new HTTPError(err.status, err.message, err))
+    res.status(200).send(data)
+  })
+})
+//////////////////////////////////////
+
+//////err handling middleware/////////
+app.use(function(err, req, res, next) {
+  console.log(req.method, req.path, err)
+  res.status(err.status || 500)
+  res.send(err)
+})
+/////////////////////////////////////
+
+//////////////////////listen//////////
+app.listen(port, () => console.log('API UP ON port:', port))
--- dal.js
+++ dal.js
@@ -1,2 +1,140 @@
 const PouchDB = require('pouchdb')
 PouchDB.plugin(require('pouchdb-find'))
+const {
+  replace,
+  pathOr,
+  assoc,
+  toLower,
+  trim,
+  split,
+  head,
+  last
+} = require('ramda')
+const db = new PouchDB(process.env.COUCHDB_URL + process.env.COUCHDB_NAME)
+const buildPrimaryKey = require('./lib/build-primary-key')
+const HTTPError = require('node-http-error')
+const paintingPKGenerator = buildPrimaryKey('painting_')
+
+//sample data
+// {
+//   _id: 'painting_starry_night',
+//   name: 'The Starry Night',
+//   type: 'painting',
+//   movement: 'post-impressionism',
+//   artist: 'Vincent van Gogh',
+//   yearCreated: 1889,
+//   museum: { name: 'Museum of Modern Art', location: 'New York' }
+// }
+//////////////CREATE A PAINTING////////////////////
+function createPainting(painting, callback) {
+  var name = pathOr('', ['name'], painting)
+  name = toLower(name)
+  //console.log('name BEFORE ramda does its sweet sweet magic', name)
+  name = replace('a', '', name)
+  name = replace('the', '', name)
+  name = trim(name)
+  //console.log('name AFTER ramda does its sweet sweet magic', name)
+  const pk = paintingPKGenerator(name)
+  //console.log('pk before assoc:', pk)
+  painting = assoc('_id', pk, painting)
+  painting = assoc('type', 'painting', painting)
+
+  createDoc(painting, callback)
+}
+
+function createDoc(doc, callback) {
+  console.log('createDoc', doc)
+
+  db.put(doc).then(res => callback(null, res)).catch(err => callback(err))
+}
+/////////////////////////////////////////////////////
+
+//////////GET A PAINTING/////////////////////
+function getPainting(paintingId, callback) {
+  db.get(paintingId, function(err, doc) {
+    if (err) return callback(err)
+    doc.type === 'painting'
+      ? callback(null, doc)
+      : callback(new HTTPError(400, 'the provided id is not a painting'))
+  })
+}
+/////////////////////////////////////////////
+
+//////////UPDATE A PAINTING//////////////////
+function updatePainting(painting, callback) {
+  painting = assoc('type', 'painting', painting)
+  createDoc(painting, callback)
+}
+/////////////////////////////////////////////
+
+////////////DELETE A PAINTING////////////////
+function deletePainting(paintingId, callback) {
+  deleteDoc(paintingId, callback)
+}
+
+function deleteDoc(id, callback) {
+  db
+    .get(id)
+    .then(function(doc) {
+      return db.remove(doc)
+    })
+    .then(function(result) {
+      callback(null, result)
+    })
+    .catch(function(err) {
+      callback(err)
+    })
+}
+/////////////////////////////////////////////
+
+///////////////////LIST PAINTINGS//////////////
+function listPaintings(filter, lastItem, limit, callback) {
+  var query = {}
+  if (filter) {
+    const arrFilter = split(':', filter)
+    const filterField = head(arrFilter)
+    const filterVal = last(arrFilter)
+    const selectorVal = assoc(filterField, filterVal, {})
+    query = {
+      selector: selectorVal,
+      limit
+    }
+  } else if (lastItem) {
+    query = {
+      selector: {
+        _id: { $gt: lastItem },
+        type: 'painting'
+      },
+      limit
+    }
+  } else {
+    query = {
+      selector: {
+        _id: { $gte: null },
+        type: 'painting'
+      },
+      limit
+    }
+  }
+  find(query, function(err, data) {
+    if (err) return callback(err)
+    callback(null, data.docs)
+  })
+}
+
+function find(query, callback) {
+  query ? db.find(query, callback) : callback(null, [])
+}
+///////////////////////////////////////////////
+
+const dal = {
+  createPainting,
+  createDoc,
+  getPainting,
+  updatePainting,
+  deletePainting,
+  deleteDoc,
+  listPaintings
+}
+
+module.exports = dal
